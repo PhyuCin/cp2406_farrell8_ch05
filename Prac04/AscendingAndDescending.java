e529d9fabdd53b3a5ee6574f51444dca7660f238
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-04-02 15:25:45 +0800

    complete AscendingAndDescending exercise

---
+++ Prac04/AscendingAndDescending.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class AscendingAndDescending {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int[] numArray = new int[3];
+
+        for(int i=0; i<3; i++) {
+            System.out.println("Enter a number: ");
+            numArray[i] = input.nextInt();
+        }
+        System.out.println("\nAscending:");
+
+        Arrays.sort(numArray);
+
+        for(int i : numArray) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\nDescending:");
+
+        for(int i= numArray.length-1; i>=0; i--){
+            System.out.print(numArray[i] + " ");
+        }
+    }
+}
