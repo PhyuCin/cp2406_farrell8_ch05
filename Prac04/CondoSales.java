57c767f9a16d519e1dd2a3102bda12c12db59a03
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-04-02 15:52:56 +0800

    complete CondoSales exercise

---
+++ Prac04/CondoSales.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CondoSales {
+    private static String[] views = {"Park View", "Golf Course View", "Lake View"};
+    private static int[] prices = {150000, 170000, 210000};
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Choose View\n1 - "+ views[0] + "\n2 - " + views[1] + "\n3 - " + views[2]);
+        try {
+            int userChoice = input.nextInt();
+            System.out.println(views[userChoice-1] + " - $" + prices[userChoice-1]);
+        }
+        catch(Exception e){
+            System.out.println("Invalid Choice - $0");
+        }
+    }
+}
